723b04259747448031424edcce9ebcc9d639a64f
Akash Sen <2207akash@gmail.com>
2024-01-24 15:31:57 +0530

    Initiated project in SwiftUI

---
+++ Todo-Check/Todo-Check/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo-Check
+//
+//  Created by Akash Sen on 24/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo-Check/Todo-Check/Todo_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_CheckApp.swift
+//  Todo-Check
+//
+//  Created by Akash Sen on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
